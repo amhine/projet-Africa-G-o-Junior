cdc6a9b1a71f30114e0f7c3438c7da294363f040
amhine <nihadamhine@gmail.com>
2024-12-13 15:59:20 +0100

    fonction de supprision des villes

--- src/deletevill.php
+++ src/deletevill.php
@@ -1,14 +1,14 @@
 
 <?php
-// include './cnxdb.php';
-// $id_ville = $_GET["id_ville"];
-// $sql = "DELETE FROM `ville` WHERE id_ville = $id_ville";
+include './cnxdb.php';
+$id_ville = $_GET["id_ville"];
+$sql = "DELETE FROM `ville` WHERE id_ville = $id_ville";
 
-// $rsulta = mysqli_query($connect, $sql);
+$rsulta = mysqli_query($connect, $sql);
 
-// if ($rsulta) {
-//   header("Location: index.php?msg=Data deleted successfully");
-// } else {
-//   echo "Failed: " . mysqli_error($connect);
-// }
+if ($rsulta) {
+  header("Location: index.php?msg=Data deleted successfully");
+} else {
+  echo "Failed: " . mysqli_error($connect);
+}
 ?>
